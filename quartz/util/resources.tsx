2cb75a9c250870bda655531c9c19cd21c02de568
때기 <102639871+hwi-ya@users.noreply.github.com>
2025-03-20 10:05:13 +0900

    Update resources.tsx

--- quartz/util/resources.tsx
+++ quartz/util/resources.tsx
@@ -40,7 +40,7 @@ export interface StaticResources {
   css: string[]
   js: JSResource[]
 }
-
+export type StringResource = string | string[] | undefined
 export function concatenateResources(...resources: StringResource[]): StringResource {
   return resources
     .filter((resource): resource is string | string[] => resource !== undefined)
