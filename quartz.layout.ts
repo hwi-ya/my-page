3bcc13d0324155ed745cf7ef46735721d09abc98
때기 <102639871+hwi-ya@users.noreply.github.com>
2025-03-20 16:55:55 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,8 +19,8 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/hwi-ya",
+      "Instagram": "https://www.instagram.com/chan._.hvvi/",
     },
   }),
 }
