03f83a9ef3183ceea92cd1058e268890142eba1d
때기 <102639871+hwi-ya@users.noreply.github.com>
2025-03-20 09:50:57 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -61,15 +61,17 @@ export const defaultListPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
+    Component.Search(),
+    Component.Darkmode(),
+    // Component.Flex({
+    //   components: [
+    //     {
+    //       Component: Component.Search(),
+    //       grow: true,
+    //     },
+    //     { Component: Component.Darkmode() },
+    //   ],
+    // }),
     Component.Explorer(),
   ],
   right: [],
