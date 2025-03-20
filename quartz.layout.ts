744e165606e4b2ed39875c42e2e46e31e2086b0a
때기 <102639871+hwi-ya@users.noreply.github.com>
2025-03-20 16:20:39 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,8 +36,17 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
+    // Component.Search(),
+    // Component.Darkmode(),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
     Component.Explorer()
   ],
   right: [
